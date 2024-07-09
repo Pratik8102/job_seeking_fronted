5d4c09549fdf9cc4ce39b0d74474d4d6650bf5cd
Pratik8102 <leetcodesharma@gmail.com>
2024-07-09 22:42:10 +0530

    added backend url

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/user/login",
+        "https://job-seeking-backend-67g5.onrender.com/api/v1/user/login",
         { email, password, role },
         {
           headers: {
--- src/components/Job/MyJobs.jsx
+++ src/components/Job/MyJobs.jsx
@@ -17,7 +17,7 @@ const MyJobs = () => {
     const fetchJobs = async () => {
       try {
         const { data } = await axios.get(
-          "http://localhost:4000/api/v1/job/getmyjobs",
+          "https://job-seeking-backend-67g5.onrender.com/api/v1/job/getmyjobs",
           { withCredentials: true }
         );
         setMyJobs(data.myJobs);
